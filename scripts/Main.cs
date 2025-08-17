7de74809dbd81688d6743ef0db7852dcba1c2aa3
Sai <saiaditya6782@gmail.com>
2025-08-17 18:45:09 -0500

    Added press q to quit functionality

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -6,6 +6,17 @@ public partial class Main : Node2D
     public override void _Ready()
     {
         base._Ready();
-
     }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventKey keyEvent && keyEvent.Pressed)
+        {
+            if (keyEvent.Keycode == Key.Q)
+            {
+                GetTree().Quit();
+            }
+        }
+    }
+
 }
