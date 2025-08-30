0d3cac3b82d5fb1924dd8681b877442485c06051
Sai <saiaditya6782@gmail.com>
2025-08-30 18:47:06 -0500

    Got piece drawing working - unrefined

--- scripts/ChessBoard.cs
+++ scripts/ChessBoard.cs
@@ -50,9 +50,12 @@ public partial class ChessBoard : Node2D
                 );
 
                 int piece = board.Square[rank * 8 + file];
-                GD.Print($"Piece: {piece} is on rank: {rank}, file: {file}.");
-                GD.Print($"SetPieceSprite: piece={piece}, texture={pieceTheme.GetPieceTexture(piece)}");
-                square.SetPieceSprite(pieceTheme.GetPieceTexture(piece));
+                Sprite2D pieceSprite = new Sprite2D();
+                pieceSprite.Texture = pieceTheme.GetPieceTexture(piece);
+                pieceSprite.Position = new Vector2(square.Position.X + 40, square.Position.Y + 40);
+                pieceSprite.Scale = new Vector2((float)0.2, (float)0.2);
+                pieceSprite.ZIndex = 3;
+                AddChild(pieceSprite);
 
                 squares[rank, file] = square;
                 AddChild(square);
--- scripts/Square.cs
+++ scripts/Square.cs
@@ -10,14 +10,12 @@ public partial class Square : Node2D
     [Export(PropertyHint.Range, "0, 7, 1")]
     public int File { get; private set; }
     private ColorRect _background;
-    private Sprite2D _pieceSprite;
 
     public override void _Ready()
     {
         base._Ready();
 
         _background = GetNode<ColorRect>("Background");
-        _pieceSprite = GetNode<Sprite2D>("PieceSprite");
     }
 
     public void Initialize(int rank, int file)
@@ -29,11 +27,6 @@ public partial class Square : Node2D
     public void SetBackgroundColor(Color color)
     {
         _background.Color = color;
-    }
-
-    public void SetPieceSprite(Texture2D pieceTexture)
-    {
-        _pieceSprite.Texture = pieceTexture;
     }
 
 }
